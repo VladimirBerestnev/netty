4b2081d2a1807cf32ba0c47c921aa7b89dd34488
Vladimir <berestnew@yandex.ru>
2022-04-28 16:11:04 +0300

    Netty - lesson 2

---
+++ Server/src/main/java/Server.java
@@ -0,0 +1,110 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    private final int port;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Server(9000).start();
+    }
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+                                        String str = "";
+
+                                        @Override
+                                        public void channelRegistered(ChannelHandlerContext ctx) {
+                                            System.out.println("channelRegistered");
+                                        }
+
+                                        @Override
+                                        public void channelUnregistered(ChannelHandlerContext ctx) {
+                                            System.out.println("channelUnregistered");
+                                        }
+
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) {
+                                            System.out.println("channelActive");
+                                        }
+
+                                        @Override
+                                        public void channelInactive(ChannelHandlerContext ctx) {
+                                            System.out.println("channelInactive");
+                                        }
+
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                            System.out.println("channelRead");
+
+//                                            ctx.writeAndFlush(msg);
+
+                                            final ByteBuf m = (ByteBuf) msg;
+
+                                            while (m.isReadable()){
+                                                char a = (char)m.readByte();
+                                                str = str + a;
+
+                                               System.out.println(str);
+                                               if (str.endsWith("/n")) {
+                                                   String outstr = str.replace("/n", "");
+
+                                                   final ByteBuf s = Unpooled.wrappedBuffer(outstr.getBytes(StandardCharsets.UTF_8));;
+                                                   ctx.writeAndFlush(s);
+                                                   str = "";
+                                               }
+                                           }
+//                                            final ByteBuf m = (ByteBuf) msg;
+//
+//                                           while (m.isReadable()){
+//                                               System.out.print((char)m.readByte());
+//                                           }
+//                                            System.out.flush();
+//                                            System.out.println();
+                                        }
+
+                                        @Override
+                                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                            System.out.println("Cause exception");
+                                            cause.printStackTrace();
+                                            ctx.close();
+                                        }
+                                    }
+                            );
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            Channel channel = server.bind(port).sync().channel();
+
+            System.out.println("Server started");
+            channel.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
